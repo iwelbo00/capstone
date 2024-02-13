a07e39d24c037febaca163b95e017f7689be1585
Ivan Welborn <113381087+iwelbo00@users.noreply.github.com>
2024-02-13 16:44:21 -0600

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
+<script setup>
+  import SideBar from "./components/SideBar.vue"
+</script>
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <VApp>
+    <SideBar></SideBar>
+    <VMain>
+      <!-- CARDS -->
+
+    </VMain>
+  </VApp>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <VNavigationDrawer expand-on-hover rail>
+        <VList>
+            <VListItem prepend-avatar="http://randomuser.me/api/portraits/men/82.jpg" title="Ivan Welborn"
+                subtitle="ivan.welborn@jbhunt.com">
+            </VListItem>
+        </VList>
+
+        <VDivider></VDivider>
+
+        <VList density="compact" nav>
+            <VListItem prepend-icon="mdi-image" title="Colored Gallery" value="color">
+
+            </VListItem>
+            <VListItem prepend-icon="mdi-dialpad" title="Grayspace Gallery" value="gray">
+
+            </VListItem>
+        </VList>
+    </VNavigationDrawer>
+</template>
